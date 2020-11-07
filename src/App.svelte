a8b1d2b2f1753e2beca94552f0ed18818e64478c
david <61949735+david-roeger@users.noreply.github.com>
2020-11-07 20:09:10 +0100

    Add svelte p5 structure

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Nav from "./components/Nav.svelte";
+	import Canvas from "./components/Canvas.svelte";
+	import shape from "./sketches/shape.js";
+	import shape2 from "./sketches/shape2.js";
+
+	import { sketches } from "./store.js";
+	let obj = [
+		{
+			name: "shape",
+			sketch: shape,
+		},
+		{
+			name: "shape2",
+			sketch: shape2,
+		},
+	];
+	$sketches = [...obj];
+</script>
+
+<style>
+	.container {
+		height: 100%;
+		display: grid;
+		background-color: black;
+		grid-template: 0 100% / 200px 1fr;
+	}
+
+	.inner-container {
+		grid-area: 1 / 2 / 2 / 3;
+		background-color: black;
+		height: 100%;
+	}
+
+	nav {
+		grid-area: 1 / 1 / 2 / 2;
+		background-color: blue;
+		height: 100%;
+	}
+</style>
+
+<div class="container">
+	<main class="inner-container">
+		<Canvas />
+	</main>
+	<nav>
+		<Nav />
+	</nav>
+</div>
---
+++ src/components/Canvas.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount, onDestroy } from "svelte";
+	import p5 from "../p5.min";
+	import { currentSketch, sketches } from "../store.js";
+
+	let p = undefined;
+	let mounted = false;
+	function setCanvas() {
+		if (mounted) {
+			const container = document.getElementById("container");
+			if (p !== undefined) {
+				p.remove();
+			}
+			p = new p5($sketches[$currentSketch].sketch);
+		}
+	}
+
+	const unsubscribe = currentSketch.subscribe(() => {
+		console.log("value changed");
+		setCanvas();
+	});
+
+	onMount(async () => {
+		mounted = true;
+		console.log("mounted");
+
+		setCanvas();
+	});
+
+	onDestroy(unsubscribe);
+</script>
+
+<style>
+	#container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
+
+<div id="container" />
