0c3bf834356c9a597edb97793952b3f7700c8a28
David Röger <david.r-oe@gmx.de>
2021-02-02 20:55:47 +0100

    Update nav

--- obskkura/scripts/nav.js
+++ obskkura/scripts/nav.js
@@ -11,23 +11,4 @@
             })
         }
     });
-
-    window.addEventListener("resize", (e) => {
-        resize()
-    })
-
-    function resize() {
-        images.forEach(image => {
-            const parent = image.parentElement;
-            const parentHeight = parent.offsetHeight;
-            const windowHeight = window.innerHeight;
-            if(windowHeight > window.innerWidth) {
-                parent.style.setProperty("margin-top", `${(windowHeight - parentHeight) / 2}px`);
-            } else {
-                parent.style.setProperty("margin-top", "0px");
-            }
-        });
-    }
-
-    resize();
 })()
